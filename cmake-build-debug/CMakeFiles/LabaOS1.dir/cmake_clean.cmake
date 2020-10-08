7a501f18964e4d90a9317d884f0f0ca00f902ad5
laslosobol <45056502+laslosobol@users.noreply.github.com>
2020-10-08 16:53:51 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/LabaOS1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LabaOS1.dir/main.cpp.obj"
+  "LabaOS1.exe"
+  "LabaOS1.exe.manifest"
+  "LabaOS1.pdb"
+  "libLabaOS1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LabaOS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/laba11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba11.dir/main.cpp.obj"
+  "laba11.exe"
+  "laba11.exe.manifest"
+  "laba11.pdb"
+  "liblaba11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
